b25785a43637f5d10a0adcdb27cc118e21e38475
Der Beta Kevin <derbetakevin@outlook.de>
2023-12-04 21:03:27 +0100

    Complete rewrite updates-nix.sh

--- Skripte/updates-nix.sh
+++ Skripte/updates-nix.sh
@@ -1,39 +1,104 @@
-#!/bin/bash
+#!/run/current-system/sw/bin/bash
 
-echo -e "\n\e[1m\e[34mStart Updates\n\n\e[31m1/10 (10%) \e[34mAppimages \e[3m(zap upgrade)\e[0m\n"
-zap upgrade
+echo -e "\n\e[1m\e[31mWas möchtest du tun?\e[0m"
+echo -e "\e[34m1. Nur das Flake updaten."
+echo -e "\e[34m2. Nur NixOS Updates anwenden."
+echo -e "\e[34m3. Nur LOKALE NixOS Konfigurationsänderungen anwenden."
+echo -e "\e[34m4. Nur Flatpaks updaten"
+echo -e "\e[34m5. Nur Distroboxen updaten"
+echo -e "\e[34m6. Updates fürs gesamte System anwenden. \e[0m"
+echo -e "\e[34m7. Updates fürs gesamte System anwenden und neustarten.\e[0m"
+echo -e "\e[34m8. Updates fürs gesamte System anwenden und herunterfahren.\e[0m"
+echo -e "\e[34m9. Nichts.\e[0m"
 
-echo -e "\n\e[1m\e[31m2/4 (50%) \e[34mFlatpaks \e[3m(flatpak update)\e[0m\n"
-flatpak update
-
-echo -e "\n\e[1m\e[31m3/4 (75%) \e[34mDistroboxes \e[3m(distrobox-upgrade -a)\e[0m\n"
-distrobox-upgrade -a
-
-echo -e "\n\e[1m\e[31m4/4 (100%) \e[34mNix - NixOS \e[3m(sudo nix-channel --update ; sudo nixos-rebuild switch --upgrade ; nix-env -u)\e[0m\n"
-sudo nix-channel --update
-sudo nixos-rebuild switch --upgrade
-nix-env -u
-
-echo -e "\n\e[1m\e[31mBefehle abgeschlossen. Was möchtest du tun?\e[0m"
-echo -e "\e[34m1. Befehl beenden\e[0m"
-echo -e "\e[34m2. PC neu starten\e[0m"
-echo -e "\e[34m3. PC herunterfahren\e[0m"
-
-read -p $'\n\e[96mBitte wähle eine Option (1-3): \e[0m' option
+read -p $'\n\e[96mWähle eine Option (1-9): \e[0m' option
 
 case $option in
   1)
-    echo "Befehl wird beendet."
+    echo -e "\n\e[1m\e[34mUpdaten des Flakes... \e[0m\n"
+    nix flake update /home/derbetakevin/Development/nixos-flakes
+    echo -e "Flake geupdated."
     ;;
   2)
-    echo "PC wird neu gestartet..."
-    sudo reboot
-    ;;
+    echo -e "\n\e[1m\e[31m1/2 (50%)Updaten des Flakes... \e[0m\n"
+    nix flake update /home/derbetakevin/Development/nixos-flakes
+    
+    echo -e "\n\e[1m\e[31m2/2 (100%)Updaten von NixOS... \e[0m\n"
+    sudo nixos-rebuild switch --flake /home/derbetakevin/Development/nixos-flakes#amdryzen
+    echo -e "NixOS geupdated."
+    ;;  
   3)
-    echo "PC wird heruntergefahren..."
-    sudo shutdown now
+    echo -e "\n\e[1m\e[34mAnwenden von LOKALEN Konfigurationsänderungen auf NixOS... \e[0m\n"
+    sudo nixos-rebuild switch --flake /home/derbetakevin/Development/nixos-flakes#amdryzen
+    echo -e "LOKALE Konfigurationsänderungen auf NixOS angewendet."
+    ;;  
+  4)
+    echo -e "\n\e[1m\e[34mUpdaten von Flakpaks... \e[0m\n"
+    flatpak update -y
+    echo -e "Flatpaks geupdated."
+    ;;
+  5)
+    echo -e "\n\e[1m\e[34mUpdaten aller Distroboxen... \e[0m\n"
+    distrobox-upgrade -a
+    echo -e "Alle Distroboxen geupdated."
+    ;;
+  6)    
+    sudo -v
+    echo -e "\n\e[1m\e[31m1/4 (25%) \e[34mUpdaten von Flatpaks... \e[0m\n"
+    flatpak update -y
+
+    echo -e "\n\e[1m\e[31m2/4 (50%) \e[34mUpdaten aller Distroboxen... \e[0m\n"
+    distrobox-upgrade -a
+
+    echo -e "\n\e[1m\e[31m3/4 (75%) \e[34mUpdaten des Flakes... \e[0m\n"
+    nix flake update /home/derbetakevin/Development/nixos-flakes
+
+    echo -e "\n\e[1m\e[31m4/4 (100%) \e[34mUpdaten von NixOS... \e[0m\n"
+    sudo nixos-rebuild switch --flake /home/derbetakevin/Development/nixos-flakes#amdryzen
+
+    echo -e "Updates angewendet."
+    ;;
+  7)
+    sudo -v
+    echo -e "\n\e[1m\e[31m1/4 (25%) \e[34mUpdaten von Flatpaks... \e[0m\n"
+    flatpak update -y
+
+    echo -e "\n\e[1m\e[31m2/4 (50%) \e[34mUpdaten aller Distroboxen... \e[0m\n"
+    distrobox-upgrade -a
+
+    echo -e "\n\e[1m\e[31m3/4 (75%) \e[34mUpdaten des Flakes... \e[0m\n"
+    nix flake update /home/derbetakevin/Development/nixos-flakes
+
+    echo -e "\n\e[1m\e[31m4/4 (100%) \e[34mUpdaten von NixOS... \e[0m\n"
+    sudo nixos-rebuild switch --flake /home/derbetakevin/Development/nixos-flakes#amdryzen
+
+    echo "Updates angewendet. Neustarten..."
+    sleep 1
+    reboot
+    ;;
+  8)
+    sudo -v
+    echo -e "\n\e[1m\e[31m1/4 (25%) \e[34mUpdaten von Flatpaks... \e[0m\n"
+    flatpak update -y
+
+    echo -e "\n\e[1m\e[31m2/4 (50%) \e[34mUpdaten aller Distroboxen... \e[0m\n"
+    distrobox-upgrade -a
+
+    echo -e "\n\e[1m\e[31m3/4 (75%) \e[34mUpdaten des Flakes... \e[0m\n"
+    nix flake update /home/derbetakevin/Development/nixos-flakes
+
+    echo -e "\n\e[1m\e[31m4/4 (100%) \e[34mUpdaten von NixOS... \e[0m\n"
+    sudo nixos-rebuild switch --flake /home/derbetakevin/Development/nixos-flakes#amdryzen
+
+    echo "Updates angewendet. Herunterfahren..."
+    sleep 1
+    poweroff
+    ;;
+  9)
+    echo "Verlassen..."
+    sleep 1
     ;;
   *)
-    echo "Ungültige Option. Der Befehl wird beendet."
-    ;;
+    echo "Ungültige Eingabe."
+    read -p $'\n\e[96mWähle eine Option (1-9): \e[0m' option
 esac
